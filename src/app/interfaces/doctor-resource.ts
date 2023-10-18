0dcf3b84595ca523950132ece0ac8fb240eba1cb
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-10-18 08:35:38 -0500

    feat: add Doctor resource interface

---
+++ src/app/interfaces/doctor-resource.ts
@@ -0,0 +1,13 @@
+export interface DoctorResource {
+    name: string;
+    email: string;
+    dni: string;
+    birthDate: string;
+    phoneNumber: string;
+    speciality: string;
+    description: string;
+    experienceYears: string;
+    patientsAssisted: string;
+    doctorFee: string;
+    profilePhoto: string;
+}
