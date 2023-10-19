61ae00d365dfe42e417f86c1faa621c86e1499a1
unknown <u201912404@upc.edu.pe>
2023-10-19 01:03:51 -0500

    feat: connection with prescription service

--- src/app/interfaces/prescriptions.ts
+++ src/app/interfaces/prescriptions.ts
@@ -1,16 +1,9 @@
 export interface Prescriptions{
   id: number;
-  idPatient: number;
-  dateIssue: string;
-  dateExpiration: string;
-  medicalSpeciality: string;
-  recipCode: string;
-  condition: string;
-  rest: string;
-  drink: string;
-  food: string;
-  medicines: object;
-  numberDose: number;
-  meals: object;
-  hours: object;
+  patientId: number;
+  doctorId: number;
+  medicineName: string;
+  medicineDosage: string;
+  medicineDuration: string;
+  date: string;
 }
